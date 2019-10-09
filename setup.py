7bd27e3be172022cdb8ac6d2cc1f9f4930c225f1
timwis <tim@timwis.com>
2019-10-09 17:04:40 +0100

    Fix setup.py and add Dockerfile

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup
 setup(
     name="code_point_open_transformer",
     version="2.0",
-    py_modules=["code_point_open_transformer"],
+    packages=["code_point_open_transformer"],
     install_requires=[
-        "click==7.0",
-        "openpyxl==3.0.0",
-        "tqdm==4.36.1",
-        "xlrd==1.2.0",
+        "click",
+        "openpyxl",
+        "tqdm",
+        "xlrd",
     ],
-    dependency_links=[],
     entry_points="""
       [console_scripts]
       codepointopen=code_point_open_transformer.cli:main
